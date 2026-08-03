fdb7770048fcdf0063a86cc93512adf7b310d41e
qgt7zm <qgt7zm@virginia.edu>
2026-08-02 20:47:24 -0400

    Fix compose file indentation and duplicate build

--- setup_docker.sh
+++ setup_docker.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 # Build the docker image and create the docker container
 
-docker build -t django_docker . &&
-    docker compose up -d --build &&
-    docker compose exec django-web python manage.py migrate &&
+docker compose up -d --build &&
+    docker compose exec web python manage.py migrate &&
     echo "Done! View the website at http://localhost:8000" ||
     echo "Build finished with errors"
